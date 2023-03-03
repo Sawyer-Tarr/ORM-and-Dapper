2a08109bc6aca1954075f97a655f41ba48487a45
sawyer tarr <sawyertarr@outlook.com>
2023-03-03 13:44:02 -0600

    AddedProductRepo

---
+++ ORM/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+
+
+        public void CreateProduct(string name, double price, int categoryID);
+       
+
+    }
+}
--- ORM/Program.cs
+++ ORM/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 using ORM;
 using System.Data;
+using System.Net.Http.Headers;
 
 var config = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
@@ -16,15 +17,41 @@ string connString = config.GetConnectionString("DefaultConnection");
 IDbConnection conn = new MySqlConnection(connString);
 
 
-var departmentRepo = new DapperDepartmentRepository(conn);
+//var departmentRepo = new DapperDepartmentRepository(conn);
 
-departmentRepo.InsertDepartment("Sawyer's New Department");
+//departmentRepo.InsertDepartment("Sawyer's New Department");
 
-var departments = departmentRepo.GetAllDepartments();
+//var departments = departmentRepo.GetAllDepartments();
 
-foreach (var department in departments)
+//foreach (var department in departments)
+//{
+//    Console.WriteLine(department.DepartmentID);
+//    Console.WriteLine(department.Name);
+//    Console.WriteLine();
+//}
+
+var productRepo = new DapperProductRepository(conn);
+
+Console.WriteLine("What is the name of your new product?");
+var productName = Console.ReadLine();
+
+Console.WriteLine("What is the price?");
+var productPrice = double.Parse(Console.ReadLine());
+
+Console.WriteLine("What is the category ID?");
+var productID = int.Parse(Console.ReadLine());
+
+productRepo.CreateProduct(productName, productPrice, productID);
+
+var products = productRepo.GetAllProducts();
+
+foreach (var item in products)
 {
-    Console.WriteLine(department.DepartmentID);
-    Console.WriteLine(department.Name);
+    Console.WriteLine(item.ProductID);
+    Console.WriteLine(item.Name);
+    Console.WriteLine(item.Price);
+    Console.WriteLine(item.CategoryID);
+    Console.WriteLine(item.OnSale);
+    Console.WriteLine(item.StockLevel);
     Console.WriteLine();
 }
